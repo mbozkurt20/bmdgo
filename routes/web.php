80c9a067737911fe69b768de7fe3c7689e7f1724
buraksyler <168175130+buraksyler@users.noreply.github.com>
2024-11-18 19:16:32 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::get('/restaurant/categories', [CategoryController::class, 'index'])->name
 Route::get('/restaurant/products', [ProductController::class, 'index'])->name('restaurant.products');
 Route::get('/restaurant/couriers', [CourierController::class, 'index'])->name('restaurant.couriers');
 Route::get('/restaurant/delivered-orders', [OrderController::class, 'deliveredOrders'])->name('restaurant.deliveredOrders');
+Route::get('/restaurant/deleted-orders', [OrderController::class, 'deletedOrders'])->name('restaurant.deletedOrders');
+
 
 
 
