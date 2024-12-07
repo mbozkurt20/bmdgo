2bc6dd973e39e440b04359e35fc8f2b64885deba
mbozkurt <mbozkurt020@hotmail.com>
2024-12-07 23:16:23 +0300

    order notification

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers\Api;
 
+use App\Events\OrderNotification;
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
 use App\Models\Order;
@@ -23,7 +24,7 @@ class OrderController extends Controller
         if ($restaurant){
             $items = [];
             if(isset($order['items'])){
-                foreach ($order['items'] as $product) { 
+                foreach ($order['items'] as $product) {
                     $addItem = [];
                     $addItem['price'] = $product['price'];
                     $addItem['count'] = $product['count'];
@@ -31,9 +32,9 @@ class OrderController extends Controller
                     $addItem['name'] = $product['name'];
                     $addItem['items'][0]['packageItemId'] = "ahtaPOS";
                     $items[] = $addItem;
-                } 
+                }
             }
-            
+
             $orderData = [
                 'platform'       => $this->enrichmentPlatform($order['provider']['slug']),
                 'courier_id'     => 0,
@@ -51,12 +52,14 @@ class OrderController extends Controller
             $createOrder = Order::create($orderData);
             if ($createOrder){
                 AssignOrderToCourier::dispatch($createOrder);
+                event(new OrderNotification($order));
+
                 return response()->json(['message' => 'Sipariş oluşturuldu'], 200);
             }
         }
-        
+
         return response()->json(['message' => 'Sipariş oluşturulamadı!'], 422);
-        
+
 
     }
 
@@ -65,13 +68,13 @@ class OrderController extends Controller
         return isset($platforms[$slug]) ?  $platforms[$slug] : $slug;
     }
 
-        
+
     public function  getPaymentMethod($slug, $id){
-        $paymentMethods=["ys"=>[["id"=>"1","name"=>"Kapıda Nakit ile Ödeme"],["id"=>"2","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"3","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"4","name"=>"Online Kredi/Banka Kartı"],["id"=>"5","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"6","name"=>"Kapıda Kredi Kartı ile Ödeme"],["id"=>"10","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"11","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"15","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"18","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"23","name"=>"Online Kredi/Banka Kartı"],["id"=>"24","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"25","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"26","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"27","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"28","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"29","name"=>"Online Kredi/Banka Kartı"]],"ty"=>[["id"=>"1","name"=>"Online Kredi/Banka Kartı"],["id"=>"2","name"=>"Online Kredi/Banka Kartı"],["id"=>"3","name"=>"Online Kredi/Banka Kartı"],["id"=>"4","name"=>"Online Kredi/Banka Kartı"]],"getir"=>[["id"=>"1","name"=>"Online Kredi/Banka Kartı"],["id"=>"2","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"3","name"=>"Kapıda Kredi Kartı ile Ödeme"],["id"=>"4","name"=>"Kapıda Nakit ile Ödeme"],["id"=>"5","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"6","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"7","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"8","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"9","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"10","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"11","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"12","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"15","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"16","name"=>"Online Kredi/Banka Kartı"],["id"=>"17","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"18","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"19","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"21","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"22","name"=>"Online Kredi/Banka Kartı"],["id"=>"24","name"=>"Online Kredi/Banka Kartı"]],"migros"=>[["id"=>1,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>2,"name"=>"Online Kredi/Banka Kartı"],["id"=>3,"name"=>"Online Kredi/Banka Kartı"],["id"=>4,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>5,"name"=>"Kapıda Kredi Kartı ile Ödeme"],["id"=>6,"name"=>"Kapıda Nakit ile Ödeme"],["id"=>7,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>8,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>9,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>10,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>11,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>12,"name"=>"Kapıda Ticket ile Ödeme"]]];        
+        $paymentMethods=["ys"=>[["id"=>"1","name"=>"Kapıda Nakit ile Ödeme"],["id"=>"2","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"3","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"4","name"=>"Online Kredi/Banka Kartı"],["id"=>"5","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"6","name"=>"Kapıda Kredi Kartı ile Ödeme"],["id"=>"10","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"11","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"15","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"18","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"23","name"=>"Online Kredi/Banka Kartı"],["id"=>"24","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"25","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"26","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"27","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"28","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"29","name"=>"Online Kredi/Banka Kartı"]],"ty"=>[["id"=>"1","name"=>"Online Kredi/Banka Kartı"],["id"=>"2","name"=>"Online Kredi/Banka Kartı"],["id"=>"3","name"=>"Online Kredi/Banka Kartı"],["id"=>"4","name"=>"Online Kredi/Banka Kartı"]],"getir"=>[["id"=>"1","name"=>"Online Kredi/Banka Kartı"],["id"=>"2","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"3","name"=>"Kapıda Kredi Kartı ile Ödeme"],["id"=>"4","name"=>"Kapıda Nakit ile Ödeme"],["id"=>"5","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"6","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"7","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"8","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"9","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"10","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"11","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"12","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"15","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"16","name"=>"Online Kredi/Banka Kartı"],["id"=>"17","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"18","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"19","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"21","name"=>"Kapıda Ticket ile Ödeme"],["id"=>"22","name"=>"Online Kredi/Banka Kartı"],["id"=>"24","name"=>"Online Kredi/Banka Kartı"]],"migros"=>[["id"=>1,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>2,"name"=>"Online Kredi/Banka Kartı"],["id"=>3,"name"=>"Online Kredi/Banka Kartı"],["id"=>4,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>5,"name"=>"Kapıda Kredi Kartı ile Ödeme"],["id"=>6,"name"=>"Kapıda Nakit ile Ödeme"],["id"=>7,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>8,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>9,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>10,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>11,"name"=>"Kapıda Ticket ile Ödeme"],["id"=>12,"name"=>"Kapıda Ticket ile Ödeme"]]];
         if ($paymentMethods[$slug]){
             $column = array_column($paymentMethods[$slug], "id");
             $index = array_search($id, $column);
-                
+
             if ($index !== false) {
                 $result = $paymentMethods[$slug][$index]['name'];
                 return $result;
